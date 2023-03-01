f7131cd565d648e4a1cc7edfd2c0b6a26f4f6cad
Uwe Steinmann <steinm@debian.org>
2023-03-01 19:45:19 +0100

    add missing phrases

--- lang.php
+++ lang.php
@@ -1,6 +1,10 @@
 <?php
 $__lang['en_GB'] = array(
+	'webdav_checkout_download'=>'Download',
+	'webdav_checkout_mimetype_match'=>'The uploaded file must have the same mimetype like the currently checked out document.',
 );
 $__lang['de_DE'] = array(
+	'webdav_checkout_download'=>'Download',
+	'webdav_checkout_mimetype_match'=>'Die hochgeladene Datei muss den gleichen Mime-Type haben wie das zur Zeit ausgecheckte Dokument.',
 );
 ?>
